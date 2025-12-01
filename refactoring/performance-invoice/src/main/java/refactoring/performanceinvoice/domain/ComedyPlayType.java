0e6d09ab09a6885a3bcad57bc355c8ff4fe99167
Zhong Jing <zhongjinggz@163.com>
2025-12-01 18:29:15 +0800

    - 提取魔法数字为常量，提高代码可读性 - 简化基础票价与观众数量的计算方式

--- refactoring/performance-invoice/src/main/java/refactoring/performanceinvoice/domain/ComedyPlayType.java
+++ refactoring/performance-invoice/src/main/java/refactoring/performanceinvoice/domain/ComedyPlayType.java
@@ -1,24 +1,33 @@
 package refactoring.performanceinvoice.domain;
 
 public class ComedyPlayType extends PlayType {
+
+    public static final int BASE_PRICE = 30000;
+    public static final int PRICE_THRESHOLD = 20;
+    public static final int EXTRA_BASE_PRICE = 10000;
+    public static final int EXTRA_UNIT_PRICE = 500;
+    public static final int UNIT_PRICE = 300;
+
+    public static final int POINTS_THRESHOLD = 30;
+    public static final int POINTS_UNIT = 5;
+
     public ComedyPlayType(String name) {
         super(name);
     }
 
     @Override
     public int calAmount(int audienceCount) {
-        int amount = 30000;
-        if (audienceCount > 20) {
-            amount += 10000 + 500 * (audienceCount - 20);
+        int amount = BASE_PRICE + UNIT_PRICE * audienceCount;
+
+        if (audienceCount > PRICE_THRESHOLD) {
+            amount += EXTRA_BASE_PRICE + EXTRA_UNIT_PRICE * (audienceCount - PRICE_THRESHOLD);
         }
-        amount += 300 * audienceCount;
         return amount;
     }
 
     @Override
     public int calAudiencePoints(int audienceCount) {
-        int points = Math.max(audienceCount - 30, 0);
-        points += Math.floorDiv(audienceCount, 5);
-        return points;
+        return Math.max(audienceCount - POINTS_THRESHOLD, 0)
+            + Math.floorDiv(audienceCount, POINTS_UNIT);
     }
 }
