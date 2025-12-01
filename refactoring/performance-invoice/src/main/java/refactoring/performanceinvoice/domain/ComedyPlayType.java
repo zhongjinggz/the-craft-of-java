9f228815749244cfa514e04cc56e6e2ec04cabe8
Zhong Jing <zhongjinggz@163.com>
2025-12-01 18:02:48 +0800

    refactor(domain): 优化喜剧类型计算逻辑 - 移除不必要的条件判断，因为此类只处理喜剧类型

--- refactoring/performance-invoice/src/main/java/refactoring/performanceinvoice/domain/ComedyPlayType.java
+++ refactoring/performance-invoice/src/main/java/refactoring/performanceinvoice/domain/ComedyPlayType.java
@@ -1,37 +1,24 @@
 package refactoring.performanceinvoice.domain;
 
-public class ComedyPlayType extends PlayType{
+public class ComedyPlayType extends PlayType {
     public ComedyPlayType(String name) {
         super(name);
     }
 
     @Override
     public int calAmount(int audienceCount) {
-        int amount;
-
-        if (name.equals("tragedy")) {
-            amount = 40000;
-            if (audienceCount > 30) {
-                amount += 1000 * (audienceCount - 30);
-            }
-        } else if (name.equals("comedy")) {
-            amount = 30000;
-            if (audienceCount > 20) {
-                amount += 10000 + 500 * (audienceCount - 20);
-            }
-            amount += 300 * audienceCount;
-        } else {
-            throw new IllegalArgumentException("戏剧类型不正确!");
+        int amount = 30000;
+        if (audienceCount > 20) {
+            amount += 10000 + 500 * (audienceCount - 20);
         }
+        amount += 300 * audienceCount;
         return amount;
     }
 
     @Override
     public int calAudiencePoints(int audienceCount) {
         int points = Math.max(audienceCount - 30, 0);
-        if ("comedy".equals(name)) {
-            points += Math.floorDiv(audienceCount, 5);
-        }
+        points += Math.floorDiv(audienceCount, 5);
         return points;
     }
 }
