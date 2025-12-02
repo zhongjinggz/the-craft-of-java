08fb2f8f706b4ba4d85ab1a10447197e48567f83
Zhong Jing <zhongjinggz@163.com>
2025-12-02 09:48:47 +0800

    增加重构注释

--- refactoring/performance-invoice/src/main/java/refactoring/performanceinvoice/application/PerformanceInvoiceService.java
+++ refactoring/performance-invoice/src/main/java/refactoring/performanceinvoice/application/PerformanceInvoiceService.java
@@ -6,8 +6,18 @@ import refactoring.performanceinvoice.domain.performanceinvoice.PerformanceInvoi
 import refactoring.performanceinvoice.domain.playtype.Play;
 import refactoring.performanceinvoice.domain.playtype.PlayTypeRepository;
 
+//DONE: 神秘命名
+//DONE：不当注释
+//DONE: 复杂循环
+//DONE: 复杂度高
+//DONE: 重复的Switch
+//DONE: 特性依恋
+//DONE: 基本类型偏执
+//DONE: 过长函数：让账单自己计算总金额和总积分
+//DONE: 魔法数字
+//DONE: plays 不应每次hardcode初始化
+//DONE: 调用链 play.getType().calAmount(perf.getAudience());
 //DONE 清理其他类
-//DOING 过长函数：让账单自己计算总金额和总积分
 
 @Service
 public class PerformanceInvoiceService {
