e6c1966c1d5c47bc41bd445ec2615491e4ab3590
Zhong Jing <zhongjinggz@163.com>
2025-12-07 12:23:33 +0800

    优化：将取Play的逻辑放入 Repository 1）重构手法：提炼函数 2）创建 PlayRepository 3）重构手法：搬移函数

--- refactoring/performance-invoice/src/main/java/refactoring/performanceinvoice/application/PerformanceInvoiceService.java
+++ refactoring/performance-invoice/src/main/java/refactoring/performanceinvoice/application/PerformanceInvoiceService.java
@@ -1,6 +1,5 @@
 package refactoring.performanceinvoice.application;
 
-import org.springframework.boot.jdbc.init.PlatformPlaceholderDatabaseDriverResolver;
 import refactoring.performanceinvoice.domain.PerformanceInvoice;
 import refactoring.performanceinvoice.domain.Play;
 import refactoring.performanceinvoice.drivenadapter.PerformanceInvoiceRepository;
@@ -17,19 +16,20 @@ public class PerformanceInvoiceService {
 
     public PerformanceInvoiceService(PerformanceInvoiceRepository repository) {
         this.repository = repository;
+        playRepository = new PlayRepository();
     }
 
 
     Map<String, Play> plays = new HashMap<>();
     public PerformanceInvoice createInvoice(PerformanceSummary performanceSummary) {
-        initPlays();
+        playRepository.initPlays();
 
         PerformanceInvoice invoice = new PerformanceInvoice(
                 performanceSummary.getCustomerName());
 
 
         for (Performance perf : performanceSummary.getPerformances()) {
-            Play play = findPlayById(perf.getPlayId());
+            Play play = playRepository.findPlayById(perf.getPlayId());
 
             invoice.addItem(play
                 , play.calAmount(perf)
@@ -41,17 +41,6 @@ public class PerformanceInvoiceService {
 
         repository.save(invoice);
         return invoice;
-    }
-
-    private Play findPlayById(String playId) {
-        return plays.get(playId);
-    }
-
-    private void initPlays() {
-        //初始化戏剧列表
-        plays.put("dasheng", new Play("dasheng", "大圣娶亲", "tragedy"));
-        plays.put("007", new Play("007", "国产凌凌漆", "comedy"));
-        plays.put("qiuxiang", new Play("qiuxiang", "唐伯虎点秋香", "comedy"));
     }
 
 }
--- refactoring/performance-invoice/src/main/java/refactoring/performanceinvoice/drivenadapter/PlayRepository.java
+++ refactoring/performance-invoice/src/main/java/refactoring/performanceinvoice/drivenadapter/PlayRepository.java
@@ -1,4 +1,21 @@
 package refactoring.performanceinvoice.drivenadapter;
 
+import refactoring.performanceinvoice.domain.Play;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class PlayRepository {
+    Map<String, Play> plays = new HashMap<>();
+
+    public void initPlays() {
+        //初始化戏剧列表
+        plays.put("dasheng", new Play("dasheng", "大圣娶亲", "tragedy"));
+        plays.put("007", new Play("007", "国产凌凌漆", "comedy"));
+        plays.put("qiuxiang", new Play("qiuxiang", "唐伯虎点秋香", "comedy"));
+    }
+
+    public Play findPlayById(String playId) {
+        return plays.get(playId);
+    }
 }
