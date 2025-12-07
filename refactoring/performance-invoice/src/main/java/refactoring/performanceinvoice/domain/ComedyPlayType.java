081ca437bfd7d855236dcafe35e95f2774e259c3
Zhong Jing <zhongjinggz@163.com>
2025-12-07 11:10:10 +0800

    坏味道：重复的Switch （Play#calAmount()和 play#calPoints() 中都包含对 playType#name的判断） 坏味道：复杂代码（Play#calAmount()中的分支条件较多） 重构手法：重构到策略模式 1）重构手法：提炼超类 2）重构手法：重命名 3）将ComedyPlayType 拷贝成 TragedyPlayType 4）编写创建 PlayType 的工厂方法（重构手法：提炼函数/重写算法） 5）在ComedyPlayType 中删除悲剧的相关算法

--- refactoring/performance-invoice/src/main/java/refactoring/performanceinvoice/domain/ComedyPlayType.java
+++ refactoring/performance-invoice/src/main/java/refactoring/performanceinvoice/domain/ComedyPlayType.java
@@ -9,9 +9,7 @@ public class ComedyPlayType extends PlayType {
     @Override
     public int calPoints(int audienceCount) {
         int points = Math.max(audienceCount - 30, 0);
-        if ("comedy".equals(getName())) {
-            points += Math.floorDiv(audienceCount, 5);
-        }
+        points += Math.floorDiv(audienceCount, 5);
         return points;
     }
 
@@ -19,20 +17,12 @@ public class ComedyPlayType extends PlayType {
     public int calAmount(int audience) {
         int amount;
 
-        if (getName().equals("tragedy")) {
-            amount = 40000;
-            if (audience > 30) {
-                amount += 1000 * (audience - 30);
-            }
-        } else if (getName().equals("comedy")) {
-            amount = 30000;
-            if (audience > 20) {
-                amount += 10000 + 500 * (audience - 20);
-            }
-            amount += 300 * audience;
-        } else {
-            throw new IllegalArgumentException("戏剧类型不正确!");
+
+        amount = 30000;
+        if (audience > 20) {
+            amount += 10000 + 500 * (audience - 20);
         }
+        amount += 300 * audience;
         return amount;
     }
 }
