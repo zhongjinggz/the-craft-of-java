fc75bf587e2819cd4a5b02774038c1c9d64f7105
Zhong Jing <zhongjinggz@163.com>
2025-12-07 11:44:36 +0800

    避免重复创建 PlayType 1) 创建静态类 PlayTypeFactory 2) 搬移函数

--- refactoring/performance-invoice/src/main/java/refactoring/performanceinvoice/domain/PlayTypeFactory.java
+++ refactoring/performance-invoice/src/main/java/refactoring/performanceinvoice/domain/PlayTypeFactory.java
@@ -1,10 +1,12 @@
 package refactoring.performanceinvoice.domain;
 
 public class PlayTypeFactory {
+    private static final PlayType TRAGEDY = new TragedyPlayType();
+    private static final PlayType COMEDY = new ComedyPlayType();
     static PlayType buildPlayType(String typeName) {
         return switch (typeName) {
-            case TragedyPlayType.NAME -> new TragedyPlayType();
-            case ComedyPlayType.NAME -> new ComedyPlayType();
+            case TragedyPlayType.NAME -> TRAGEDY;
+            case ComedyPlayType.NAME -> COMEDY;
             default -> throw new IllegalArgumentException("戏剧类型不正确!");
         };
     }
