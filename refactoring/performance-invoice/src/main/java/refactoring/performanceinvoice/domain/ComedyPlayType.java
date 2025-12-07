5908908a87deb17f1fc1f29371a40c37d1d91777
Zhong Jing <zhongjinggz@163.com>
2025-12-07 11:24:17 +0800

    坏味道：魔法数字 重构手法：提炼常量 1）提炼 TragedyPlayType 中的常量 2）提炼 ComedyPlayType 中的常量

--- refactoring/performance-invoice/src/main/java/refactoring/performanceinvoice/domain/ComedyPlayType.java
+++ refactoring/performance-invoice/src/main/java/refactoring/performanceinvoice/domain/ComedyPlayType.java
@@ -2,21 +2,30 @@ package refactoring.performanceinvoice.domain;
 
 public class ComedyPlayType extends PlayType {
 
+    private static final int POINTS_THRESHOLD = 30;
+    private static final int POINTS_UNIT = 5;
+    private static final int AMOUNT_THRESHOLD = 20;
+    private static final int BASE_PRICE = 30000;
+    private static final int UNIT_PRICE = 300;
+    private static final int EXTRA_BASE_PRICE = 10000;
+    private static final int EXTRA_UNIT_PRICE = 500;
+
     public ComedyPlayType(String name) {
         super(name);
     }
 
     @Override
     public int calPoints(int audienceCount) {
-        return Math.max(audienceCount - 30, 0)
-            + Math.floorDiv(audienceCount, 5);
+        return Math.max(audienceCount - POINTS_THRESHOLD, 0)
+            + Math.floorDiv(audienceCount, POINTS_UNIT);
     }
 
     @Override
     public int calAmount(int audience) {
-        int amount = 30000 + 300 * audience;
-        if (audience > 20) {
-            amount += 10000 + 500 * (audience - 20);
+        int amount = BASE_PRICE + UNIT_PRICE * audience;
+        if (audience > AMOUNT_THRESHOLD) {
+            amount += EXTRA_BASE_PRICE
+                + EXTRA_UNIT_PRICE * (audience - AMOUNT_THRESHOLD);
         }
         return amount;
     }
